e56784b32dd8b09ff11d6eb83fbad0013c2f012a
Carl Hain <crhain@gmail.com>
2016-10-12 18:45:27 -0500

    Create basic react app

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
+import React from 'react'; //es6 version
+//var React = require('react');
+import { render } from 'react-dom'; //es6 version
+//var render = require('react-dom').render;
+import  App from '../components/App';
 
+render(
+	// define the econompassing component,
+	// DOM element we want to mount it to
+	<App/>,
+	document.getElementById('app')
+);
