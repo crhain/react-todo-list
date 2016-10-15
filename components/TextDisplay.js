992e19377dd70d542570aa7a78caf7cc9d88fe1d
Carl Hain <crhain@gmail.com>
2016-10-15 16:41:17 -0500

    Add text input and updating display

---
+++ components/TextDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TextDisplay extends Component{
+
+	constructor(props, context){
+		super(props, context);
+	}
+
+	render(){
+		return <div>I'm displaying text: {this.props.text}</div>;
+	}
+}
+
+export default TextDisplay;
